1366ff50e43f929ce71342f39936dbf50c72a05f
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2022-06-14 08:40:58 +0200

    Add more info to __init__.py
    Former-commit-id: b6a8d10e6536d16913f06bb9708bb681f892cfa5

--- causalinflation/__init__.py
+++ causalinflation/__init__.py
@@ -1,3 +1,14 @@
+'''CausalInflation
+=====
+Provides
+ 1. xxxx
+ 2. xxxx
+'''
+
+__version__ = '0.1'
+
 from .InflationProblem import InflationProblem
 from .quantum.InflationSDP import InflationSDP
 
+__all__ = ['InflationProblem',
+           'InflationSDP']
