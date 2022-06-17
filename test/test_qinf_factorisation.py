e3671ffbf12f9a2fb383fd07637a5120be021ee3
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2022-06-17 14:24:59 +0200

    Adapt tests to new package structure

--- test/test_qinf_factorisation.py
+++ test/test_qinf_factorisation.py
@@ -1,6 +1,7 @@
 import unittest
+import numpy as np
 
-from causalinflation.general_tools import *
+from causalinflation.quantum.general_tools import factorize_monomial
 
 class QuantumInflationFactorization(unittest.TestCase):
 
--- test/test_torepr.py
+++ test/test_torepr.py
@@ -1,7 +1,7 @@
 import unittest
 
-from causalinflation.general_tools import *
-from causalinflation.fast_npa import *
+from causalinflation.quantum.general_tools import *
+from causalinflation.quantum.fast_npa import *
 
 class TestToRepr(unittest.TestCase):
     def test_to_representative(self):
@@ -15,4 +15,3 @@ class TestToRepr(unittest.TestCase):
         result = to_name(to_representative(np.array(to_numbers(monomial_string, names)), np.array([2, 2, 2])), names)
         correct = 'A_1_1_0_0_0*A_2_2_0_0_0*B_1_0_1_0_0*B_2_0_2_0_0'
         self.assertEqual(result, correct, "Problem with bringing monomial to representative form")
-        
