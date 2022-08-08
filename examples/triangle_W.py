d3e92bbd70bca3a498c8c8be7e589a0a20216022
Cristian <ecboghiu@gmail.com>
2022-08-08 11:46:27 +0200

    Added examples
    Former-commit-id: efa45702bf1a09d83f107046a7a29a9b437c9361

--- examples/triangle_W.py
+++ examples/triangle_W.py
@@ -1,5 +1,6 @@
 from causalinflation import InflationProblem, InflationSDP
 from examples_utils import bisection, P_W_array
+import numpy as np
 
 qtriangle = InflationProblem(dag={"rho_AB": ["A", "B"],
                                   "rho_BC": ["B", "C"],
@@ -9,11 +10,18 @@ qtriangle = InflationProblem(dag={"rho_AB": ["A", "B"],
                              inflation_level_per_source=[2, 2, 2])
 sdprelax = InflationSDP(qtriangle, verbose=1)
 
-cols = sdprelax.build_columns('physical2', max_monomial_length=4)
+cols = sdprelax.build_columns('npa2', max_monomial_length=4)
 sdprelax.generate_relaxation(cols)
 
+mon1 = sdprelax._monomials_list_all[2:10,1]
+mon2 = sdprelax._monomials_list_all[3:5,1]
 
-# sdprelax.set_distribution(P_W_array)
-# sdprelax.solve()
+sdprelax.moment_lowerbounds = {mon: -1 for mon in mon1}
+sdprelax.moment_upperbounds = {mon: 3 for mon in mon2}
 
-bisection(sdprelax, P_W_array, 1e-4, verbose=True)
+# v = 0.9
+# sdprelax.set_distribution(v*P_W_array+(1-v)*np.ones((2,2,2,1,1,1))/8,  use_lpi_constraints=True)
+# sdprelax.solve(dualise=True)
+# print(sdprelax.status)
+
+bisection(sdprelax, P_W_array, 1e-6, verbose=True)
