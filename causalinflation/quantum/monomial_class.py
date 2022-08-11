8c3ad97c0091b62b744e7dc3983910a8ff4a49e4
Elie Wolfe <elie@causarum.org>
2022-08-11 15:11:52 -0400

    Attempting to use legacy MOSEK interface. Identified bug in my implementation of apply_inflation_symmetries, however.

--- causalinflation/quantum/monomial_class.py
+++ causalinflation/quantum/monomial_class.py
@@ -5,19 +5,31 @@ import numpy as np
 from causalinflation.quantum.general_tools import factorize_monomial, is_physical, is_knowable  # to_representative_aux
 # import itertools
 from functools import cached_property
+from functools import lru_cache
 
 from typing import List, Tuple, Union
+from collections.abc import Iterable
 # ListOrTuple = NewType("ListOrTuple", Union[List, Tuple])
 # MonomInputType = NewType("NumpyCompat", Union[np.ndarray, ListOrTuple[ListOrTuple[int]]])
 
-def to_tuple_of_tuples(monomial: np.ndarray) -> Tuple[Tuple[int]]:
-    if monomial.ndim >= 2:
-        return tuple(to_tuple_of_tuples(operator) for operator in monomial)
-    elif monomial.ndim == 1:
-        return tuple(monomial.tolist())
+def to_tuple_of_tuples(monomial):
+    if isinstance(monomial, tuple):
+        return monomial
+    elif isinstance(monomial, np.ndarray):
+        if monomial.ndim >= 2:
+            return tuple(to_tuple_of_tuples(operator) for operator in monomial)
+        elif monomial.ndim == 1:
+            return tuple(monomial.tolist())
+        else:
+            return monomial.tolist()
+    elif isinstance(monomial, Iterable):
+        return to_tuple_of_tuples(np.array(monomial))
     else:
         return monomial
 
+@lru_cache(maxsize=None, typed=False)
+def compute_marginal_memoized(prob_array: Tuple, atom: Tuple[Tuple[int]]) -> float:
+    return compute_marginal(np.asarray(prob_array), np.asarray(atom))
 
 def compute_marginal(prob_array: np.ndarray, atom: np.ndarray) -> float:
     """Function which, given an atomic monomial and a probability distribution prob_array
@@ -48,31 +60,12 @@ def compute_marginal(prob_array: np.ndarray, atom: np.ndarray) -> float:
     outputs = atom[:, -1].astype(int)
     indices_to_sum = list(set(range(n_parties)).difference(participating_parties))
     marginal_dist = np.sum(prob_array, axis=tuple(indices_to_sum))
-    input_list: ndarray = np.zeros(n_parties, dtype=int)
+    input_list: np.ndarray = np.zeros(n_parties, dtype=int)
     input_list[participating_parties] = inputs
     outputs_inputs = np.concatenate((outputs, input_list))
     return marginal_dist[tuple(outputs_inputs)]
 
 class Monomial(object):
-    # __frozen_atributes = [
-    #     'as_ndarray',
-    #      'n_ops',
-    #      'op_length',
-    #      'as_tuples',
-    #      'is_physical',
-    #      'factors',
-    #      'nof_factors',
-    #      'atomic_knowability_status',
-    #      'knowable_factors_uncompressed',
-    #      'knowable_factors',
-    #      'unknowable_factors',
-    #      'knowable_q',
-    #      'physical_q']
-    # __attributes_adjustable_by_user = [
-    #     'known_part_value',
-    #     'unknown_part_representative',
-    #     'unknown_part_name',
-    #     'known_part']
     __slots__ = ['as_ndarray',
                  'n_ops',
                  'op_length',
@@ -83,13 +76,21 @@ class Monomial(object):
                  'atomic_knowability_status',
                  'knowable_factors_uncompressed',
                  'knowable_factors',
+                 'knowable_factors_names',
+                 'knowable_part_name',
                  'unknowable_factors',
                  'unknowable_factors_as_block',
+                 'unknown_part_name',
                  '_unknowable_block_len',
                  'knowability_status',
                  'knowable_q',
                  'physical_q',
-                 'idx'
+                 'idx',
+                 'known_status',
+                 'known_value',
+                 'unknown_part',
+                 'representative',
+                 'name'
                  ]
     def __init__(self, array2d: Union[np.ndarray, Tuple[Tuple[int]], List[List[int]]],
                  atomic_is_knowable=is_knowable,
@@ -100,11 +101,12 @@ class Monomial(object):
         Note that if knowable_q changes (such as given partial information) we can update this on the fly.
         """
         # self.to_representative = lambda mon: tuple(tuple(vec) for vec in to_representative(mon))
+        self.idx = idx
         self.as_ndarray = np.asarray(array2d)
         assert self.as_ndarray.ndim == 2, 'Expected 2 dimension numpy array.'
         self.n_ops, self.op_length = self.as_ndarray.shape
         assert self.op_length >= 3, 'Expected at least 3 digits to specify party, outcome, settings.'
-        self.as_tuples = tuple(tuple(vec) for vec in self.as_ndarray)
+        self.as_tuples = to_tuple_of_tuples(self.as_ndarray)
         self.factors = factorize_monomial(self.as_ndarray)
         self.nof_factors = len(self.factors)
         self.atomic_knowability_status = tuple(atomic_is_knowable(atom) for atom in self.factors)
@@ -117,8 +119,11 @@ class Monomial(object):
                                                        self.atomic_knowability_status)
                                                    if not knowable)
         self.unknowable_factors_as_block = self.factors_as_block(self.unknowable_factors)
-        self.knowable_factors = tuple(tuple(tuple(vec) for vec in np.take(factor, [0, -2, -1], axis=1))
-                for factor in self.knowable_factors_uncompressed)
+        # self.knowable_factors = tuple(tuple(tuple(vec) for vec in np.take(factor, [0, -2, -1], axis=1))
+        #         for factor in self.knowable_factors_uncompressed)
+        #knowable factors must be hashable, but this is taken care of after externally rectifying fake settings.
+        self.knowable_factors = tuple(np.take(factor, [0, -2, -1], axis=1).astype(int)
+                                      for factor in self.knowable_factors_uncompressed)
         self.knowable_q = (len(self.knowable_factors) == self.nof_factors)
         self.physical_q = self.knowable_q or is_physical(self.unknowable_factors_as_block,
                                                          sandwich_positivity=sandwich_positivity)
@@ -158,7 +163,7 @@ class Monomial(object):
 
     def update_given_valuation_of_knowable_part(self, valuation_of_knowable_part):
         actually_known_factors = np.logical_not(np.isnan(valuation_of_knowable_part))
-        known_part_value = float(np.prod(np.compress(
+        self.known_value = float(np.prod(np.compress(
             actually_known_factors,
             valuation_of_knowable_part)))
         nof_known_factors = np.count_nonzero(actually_known_factors)
@@ -167,22 +172,30 @@ class Monomial(object):
                                                     actually_known_factors)
                                                 if not known]
 
-        unknown_part = np.concatenate((
+        self.unknown_part = np.concatenate((
             self.factors_as_block(knowable_factors_which_are_not_known),
             self.unknowable_factors_as_block))
-        unknown_len = len(unknown_part)
+        unknown_len = len(self.unknown_part)
         if unknown_len == 0:
-            status = 'Known'
+            self.known_status = 'Yes'
         elif unknown_len == self.n_ops:
-            status = 'Unknown'
+            self.known_status = 'No'
         else:
-            status = 'Semiknown'
-        return (known_part_value, unknown_part, status)
+            self.known_status = 'Semi'
+
+    def update_given_prob_dist(self, prob_array):
+        if prob_array is None:
+            self.known_status = 'No'
+            self.known_value = 1
+            self.unknown_part = self.as_ndarray
+        else:
+            hashable_prob_array = to_tuple_of_tuples(prob_array)
+            valuation_of_knowable_part = np.array([
+                compute_marginal_memoized(hashable_prob_array, atom)
+                for atom in self.knowable_factors])
+            return self.update_given_valuation_of_knowable_part(valuation_of_knowable_part)
 
 
-class SDPVar(Monomial):
-    def __init__(self, mon: Monomial, prob_array: np.ndarray, use_lpi_constraints=False):
-        valuation_of_knowable_part
 
 
 
