f64f62147bfb3209b3f542f043eba31a8d74df75
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2022-09-20 21:11:28 +0200

    Get InflationProblem.py ready to publish
    Names simplified, comments added, documentation completed, long lines broken

--- causalinflation/InflationProblem.py
+++ causalinflation/InflationProblem.py
@@ -1,11 +1,18 @@
 """
 The module creates the inflation scenario associated to a causal structure. See
 arXiv:1609.00672 and arXiv:1707.06476 for the original description of inflation.
-@authors: Alejandro Pozas-Kerstjens, Emanuel-Cristian Boghiu
+@authors: Emanuel-Cristian Boghiu, Elie Wolfe, Alejandro Pozas-Kerstjens
 """
 import numpy as np
 from itertools import chain
 from warnings import warn
+
+# Force warnings.warn() to omit the source code line in the message
+# Source: https://stackoverflow.com/questions/2187269/print-only-the-message-on-warnings
+import warnings
+formatwarning_orig = warnings.formatwarning
+warnings.formatwarning = lambda message, category, filename, lineno, line=None:\
+    formatwarning_orig(message, category, filename, lineno, line='')
 
 class InflationProblem(object):
     """Class for enconding relevant details concerning the causal compatibility
@@ -16,18 +23,23 @@ class InflationProblem(object):
     dag : list, optional
         Dictionary where each key is a hyperedge connecting different
         parties. By default it is a single source connecting all parties.
-    outcomes_per_party : list, optional
+    outcomes_per_party : List[int], optional
         Measurement outcome cardinalities. By default ``2`` for all parties.
-    settings_per_party : list, optional
+    settings_per_party : List[int], optional
         Measurement setting cardinalities. By default ``1`` for all parties.
-    inflation_level_per_source : list, optional
-        Number of copies per source in the inflated graph. By default ``1`` for
-        all sources.
-    order : List[int], optional
+    inflation_level_per_source : [int, List[int]], optional
+        Number of copies per source in the inflated graph. If an integer is
+        provided, it is used as the inflation level for all sources. By default
+        ``1`` for all sources.
+    order : List[str], optional
         Name of each party, default is alphabetical labels, e.g.,
         ``['A', 'B', ...]``.
     verbose : int, optional
-        How much information to print. By default ``0``.
+        Optional parameter for level of verbose:
+
+        * 0: quiet (default),
+        * 1: monitor level: track program process and see warnings,
+        * 2: debug level: show properties of objects created.
     """
 
     def __init__(self,
@@ -41,39 +53,50 @@ class InflationProblem(object):
         """
         self.verbose = verbose
 
+        # Read settings and outcomes
         if not outcomes_per_party:
             raise ValueError("Please provide outcomes per party.")
         self.outcomes_per_party = np.array(outcomes_per_party, dtype=int)
-        self.nr_parties = len(self.outcomes_per_party)
+        self.nr_parties         = len(self.outcomes_per_party)
         if not settings_per_party:
             if self.verbose > 0:
-                print("No settings per party provided, assuming all parties have one setting.")
-            self.private_settings_per_party = np.ones(self.nr_parties, dtype=int)  # [1] * self.nr_parties,
+                warn("No settings per party provided, "
+                      + "assuming all parties have one setting.")
+            self.private_settings_per_party = np.ones(self.nr_parties,
+                                                      dtype=int)
         else:
-            self.private_settings_per_party = np.asarray(settings_per_party, dtype=int)
-            assert len(
-                self.private_settings_per_party) == self.nr_parties, "Different numbers of cardinalities specified for inputs versus outputs."
+            self.private_settings_per_party = np.asarray(settings_per_party,
+                                                         dtype=int)
+            assert len(self.private_settings_per_party) == self.nr_parties,    \
+                (f"You have specified a list of {len(outcomes_per_party)} "
+                 + f"outcomes and a list of {len(settings_per_party)} inputs. "
+                 + "These lists must have the same length and equal to the "
+                 + "number of visible variables in the scenario.")
 
-        # We need to infer the names of the parties. If they are explicitly given, great. Otherwise, use DAG order.
+        # Assign names to the visible variables
         names_have_been_set_yet = False
-        if dag and (not names_have_been_set_yet):
-            implicit_names_as_set = set(chain.from_iterable(dag.values()))
-            assert (
-                        len(implicit_names_as_set) == self.nr_parties), "DAG has a different number of outcome-associated variables than" \
-                                                                        + "were given by the user-specified cardinalities."
+        if dag:
+            implicit_names = set(chain.from_iterable(dag.values()))
+            assert len(implicit_names) == self.nr_parties,                     \
+                ("You must provide a number of outcomes for the following "
+                 + f"{len(implicit_names)} variables: {implicit_names}")
             if order:
-                sanity_check = (implicit_names_as_set.issubset(order) and implicit_names_as_set.issuperset(order))
+                sanity_check = (implicit_names.issubset(order)
+                                and implicit_names.issuperset(order))
                 if sanity_check:
                     self.names = order
                     names_have_been_set_yet = True
                 else:
-                    warn(
-                        "Names read from DAG do not match names given as keyword argument. IGNORING user-specified names.")
-            if not names_have_been_set_yet:
-                if len(implicit_names_as_set) > 1:
                     if self.verbose > 0:
-                        warn("Order of variables is inferred by the DAG according to lexicographic order.")
-                self.names = sorted(implicit_names_as_set)
+                        warn("The names read from the DAG do not match those "
+                             + "read from the `order` argument. The names used "
+                             + "are those read from the DAG.")
+            if not names_have_been_set_yet:
+                if len(implicit_names) > 1:
+                    if self.verbose > 0:
+                        warn("The order of variables is inferred by the DAG "
+                             + "according to lexicographic order.")
+                self.names = sorted(implicit_names)
                 names_have_been_set_yet = True
         if order and (not names_have_been_set_yet):
             sanity_check = (len(order) == self.nr_parties)
@@ -81,64 +104,91 @@ class InflationProblem(object):
                 self.names = order
                 names_have_been_set_yet = True
             else:
-                warn("Number of names does not match the user-specified cardinalities. IGNORING user-specified names.")
+                if self.verbose > 0:
+                    warn("The number of names provided does not match the "
+                         + "number of variables that need a name. The names "
+                         + "used are determined by the list of variables "
+                         + "with outcomes.")
         if not names_have_been_set_yet:
             self.names = [chr(ord('A') + i) for i in range(self.nr_parties)]
 
         if not dag:
-            warn("Hypergraph must be a non-empty dict of lists. Defaulting to global source.")
+            if self.verbose > 0:
+                warn("The DAG must be a non-empty dictionary with parent "
+                     + "variables as keys and lists of children as values. "
+                     + "Defaulting to one global source.")
             self.dag = {'h_global': self.names}
         else:
             self.dag = dag
 
-        nodes_with_children = list(dag.keys())
-        self.has_children = np.zeros(self.nr_parties, dtype=int)
-        self.split_node_model = not set(nodes_with_children).isdisjoint(self.names)
-        names_to_integers_dict = {party: position for position, party in enumerate(self.names)}
-        adjacency_matrix = np.zeros((self.nr_parties, self.nr_parties), dtype=np.uint8)
+        # Unpacking of visible nodes with children
+        nodes_with_children = list(self.dag.keys())
+        self.has_children   = np.zeros(self.nr_parties, dtype=int)
+        self.split_node_model = (not
+                                set(nodes_with_children).isdisjoint(self.names))
+        names_to_integers = {party: position
+                             for position, party in enumerate(self.names)}
+        adjacency_matrix = np.zeros((self.nr_parties, self.nr_parties),
+                                    dtype=np.uint8)
         for parent in nodes_with_children:
             if parent in self.names:
-                ii = names_to_integers_dict[parent]
+                ii = names_to_integers[parent]
                 self.has_children[ii] = 1
                 for child in dag[parent]:
-                    jj = names_to_integers_dict[child]
+                    jj = names_to_integers[child]
                     adjacency_matrix[ii, jj] = 1
         self.parents_per_party = list(map(np.flatnonzero, adjacency_matrix.T))
-        settings_per_party_as_lists = [[s] for s in self.private_settings_per_party]
-        for (party_index, party_parents_indices) in enumerate(self.parents_per_party):
-            settings_per_party_as_lists[party_index].extend(np.take(self.outcomes_per_party, party_parents_indices))
-        self.settings_per_party = np.asarray([np.prod(multisetting) for multisetting in settings_per_party_as_lists], dtype=int)
+        settings_per_party_lst = [[s] for s in self.private_settings_per_party]
+        for party_idx, party_parents_idxs in enumerate(self.parents_per_party):
+            settings_per_party_lst[party_idx].extend(
+                                                np.take(self.outcomes_per_party,
+                                                        party_parents_idxs)
+                                                     )
+        self.settings_per_party = np.asarray([np.prod(multisetting)
+                                    for multisetting in settings_per_party_lst],
+                                             dtype=int)
 
         extract_parent_values_from_effective_setting = []
         for i in range(self.nr_parties):
             extract_parent_values_from_effective_setting.append(dict(zip(
                 range(self.settings_per_party[i]),
-                np.ndindex(tuple(settings_per_party_as_lists[i])))))
-        self.extract_parent_values_from_effective_setting = extract_parent_values_from_effective_setting
+                np.ndindex(tuple(settings_per_party_lst[i])))))
+        self.extract_parent_values_from_effective_setting = \
+            extract_parent_values_from_effective_setting
 
-        actual_sources = [source for source in nodes_with_children if source not in self.names]
+        # Create network corresponding to the unpacked scenario
+        actual_sources  = [source for source in nodes_with_children
+                           if source not in self.names]
         self.nr_sources = len(actual_sources)
-        hypergraph = np.zeros((self.nr_sources, self.nr_parties), dtype=np.uint8)
+        hypergraph      = np.zeros((self.nr_sources, self.nr_parties),
+                                   dtype=np.uint8)
         for ii, source in enumerate(actual_sources):
-            pos = [names_to_integers_dict[party] for party in dag[source]]
+            pos = [names_to_integers[party] for party in self.dag[source]]
             hypergraph[ii, pos] = 1
 
         self.hypergraph = hypergraph
 
         assert self.hypergraph.shape[1] == self.nr_parties, \
-            (f"The number of parties derived from the DAG is {self.hypergraph.shape[1]} and " +
-             f"from the specification of outcomes it is {self.nr_parties} instead")
+            ("The number of parties derived from the DAG is "
+             + f"{self.hypergraph.shape[1]} and from the specification of "
+             + f"outcomes it is {self.nr_parties} instead.")
 
-        if np.array(inflation_level_per_source).size == 0:
+        if len(inflation_level_per_source) == 0:
             if self.verbose > 0:
-                print("Inflation level per source must be a non-empty list. Defaulting to 1 (standard NPA).")
+                warn("The inflation level per source must be a non-empty list. "
+                     + "Defaulting to 1 (no inflation, just NPA hierarchy).")
             self.inflation_level_per_source = np.array([1] * self.nr_sources)
         elif type(inflation_level_per_source) == int:
-            self.inflation_level_per_source = np.array([inflation_level_per_source] * self.nr_sources)
+            self.inflation_level_per_source = \
+                np.array([inflation_level_per_source] * self.nr_sources)
         else:
-            self.inflation_level_per_source = np.array(inflation_level_per_source)
-            assert self.nr_sources == len(self.inflation_level_per_source), ("The number of sources,"
-                                                                             + " as described by the hypergraph and the list of inflation levels, does not coincide")
+            self.inflation_level_per_source = \
+                np.array(inflation_level_per_source)
+            assert self.nr_sources == len(self.inflation_level_per_source), \
+                ("The number of sources as described by the unpacked hypergraph"
+                 + f", {len(self.nr_sources)}, and by the list of inflation "
+                 + f"levels specified, {len(self.inflation_level_per_source)}, "
+                 + "does not coincide.")
 
     def __repr__(self):
         return ("InflationProblem with " + str(self.hypergraph.tolist()) +
