0411a5798426312fd3a942fc25897269500ba555
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2022-09-29 12:09:28 +0200

    Make utils.py pretty
    Former-commit-id: 3841970252aa83a28d6fa66d125b06939fb2ccd8

--- causalinflation/utils.py
+++ causalinflation/utils.py
@@ -1,4 +1,12 @@
+"""
+This file contains auxiliary functions of general purpose
+@authors: Emanuel-Cristian Boghiu, Elie Wolfe and Alejandro Pozas-Kerstjens
+"""
 def blank_tqdm(*args, **kwargs):
+    """Placeholder in case the tqdm library is not installed (see
+    https://tqdm.github.io). If code is set to print a tqdm progress bar and
+    tqdm is not installed, this just prints the ``desc`` argument.
+    """
     try:
         if not kwargs['disable']:
             print(kwargs['desc'])
