ac6ed01881b3c5851ea12512a8b8e547eeeaebb1
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2022-10-20 15:06:38 +0200

    Create bisection function
    Former-commit-id: 9d73322d8e4d498a254dc3d2eb2b232d2cdb9a37

--- causalinflation/utils.py
+++ causalinflation/utils.py
@@ -4,7 +4,7 @@ This file contains auxiliary functions of general purpose
 """
 import numpy as np
 from itertools import chain
-from typing import Iterable
+from typing import Callable, Iterable
 
 
 def blank_tqdm(*args, **kwargs):
@@ -30,3 +30,45 @@ def flatten(nested):
         while isinstance(nested[0], Iterable):
             nested = list(chain.from_iterable(nested))
         return nested
+
+
+###############################################################################
+# ROUTINES USED FOR OPTIMIZATION                                              #
+###############################################################################
+def bisect(f: Callable[[float], float],
+           x0: float,
+           x1: float,
+           eps=1e-4,
+           verbose=False) -> float:
+    r"""Find a value :math:`x\in[x0, x1]` where :math:`f(x)=0` using the
+    bisection method.
+
+    Parameters
+    ----------
+    f : function
+        The function to be evaluated. It must be a function from the real line
+        into the real line.
+    x0: float
+        The lower end of the search interval.
+    x1: float
+        The higher end of the search interval.
+    eps : float, optional
+        The stopping criterion, expressed as the width of the interval centered
+        in the output where the real solution is. By default ``1e-4``.
+    verbose: bool, optional
+        Whether information about each iteration is printed. By default
+        ``False``.
+    """
+    assert f(x0)*f(x1) < 0., \
+        "The function should have different signs in x0 and x1."
+    x = (x0 + x1) / 2
+    while abs(x1 - x0) > eps:
+        fx = f(x)
+        if fx >= 0.:
+            x0 = x
+        else:
+            x1 = x
+        if verbose:
+            print(f"{f.__name__}({x:.4g}) = {fx:10.4g}")
+        x = (x0 + x1) / 2
+    return x
