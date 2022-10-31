b588a7da1b0439b4be5dfba050a82c01662ddf82
Elie Wolfe <elie@causarum.org>
2022-10-31 16:06:42 -0400

    Remove a helper function from optimization_utils.py, showcasing my attitude towards the comments in https://github.com/ecboghiu/inflation/pull/85#issuecomment-1294817267
    Former-commit-id: eb79db40fbc9811cd89340b30d8cd19989de65c5

--- inflation/quantum/optimization_utils.py
+++ inflation/quantum/optimization_utils.py
@@ -7,7 +7,7 @@ distribution for which quantum inflation (at specific hierarchy levels) is
 """
 from typing import Callable
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize, Bounds
 from sympy import var
 from sympy.utilities.lambdify import lambdify
 
@@ -15,142 +15,75 @@ from causalinflation import InflationSDP
 from causalinflation.quantum.general_tools import make_numerical
 
 
-def symbolic_values_sdp_optimize(sdp: InflationSDP,
-                                 list_of_symbols: list,
-                                 names_to_expr_dict: dict,
-                                 objective_to_min=(lambda x: -x[0]),
-                                 x0=np.array([-1.0]),
-                                 bounds=np.array([[0.0, 1.0]]),
-                                 precision=0.0001,
-                                 verbose=False) -> dict:
-    """Minimizes a function of symbolic variables over the valuations of those
-     variables which would make the SDP feasible, leveraging the infeasibility
-    certificates yielded by the dual solution. To relate the symbolic
-    variables to the monomials which appear in the SDP the user must supply a
-    dictionary where the keys are names of monomial and the values are symbolic
-    expressions for the corresponding monomial in terms of the symbolic
-    variables. Optional bounds on the symbolic variables may be given.
-
-
-
-    Parameters
-    ----------
-    sdp: InflationSDP
-        The instance of InflationSDP upon which to iteratively call
-        set_values() and solve().
-    list_of_symbols: list
-        A list of the symbolic variables in terms of which all
-        symbolic expressions are defined in terms of.
-    names_to_expr_dict: dict
-        A dictionary where the keys are names of monomial and the values are
-        symbolic expressions for the corresponding monomial in terms of the
-        symbolic variables.
-    objective_to_min: multivariate function, optional
-        The function to minimize. If not specified this is taken to be equal to
-        minus the value of the first symbolic variable in the list specified in
-        the second argument.
-    x0: numpy.ndarray, optional
-        The initial numerical values to use for the symbolic variables. Should
-        be chosen to make the SDP initially infeasible. When unspecified,
-        defaults to assuming a single symbolic variable with an initial
-        numerical value of 1.
-    bounds: numpy.ndarray, optional
-        An array up values pairs, indicating respectively the upper and lower
-        numerical values to be considered for each of the symbolic variables.
-        When unspecified, defaults to assuming a single symbolic variable with
-        a numerical range between 0 and 1.
-    precision: float, optional
-        The iterative exploration of numerical values for the symbolic variables
-        ceases if rejected range for the objective to be minimized does not
-        increase by at least this value between iterations. If unspecified, the
-        precision is set to 1e-4. May be set to zero.
-    verbose: bool, optional
-        If True, displays the updated numerical value of the function to be
-        minimized every time solve() is called for the InflationSDP.
-
-    Returns
-    -------
-        A dictionary where the key 'best_feasible_values' is associated with a
-        numpy array of float which specify values for the symbolic variables
-        which would make the SDP feasible (or which at least would not violate
-        the final certificate). The key 'certificate' returns to most recent
-        certificate, given in the form of a symbolic expression (a polynomial
-        function of the symbolic variables). The key 'precision' return the
-        precision value.
-    """
-    plural = ''
-    if len(list_of_symbols) > 1:
-        plural = 's'
-    new_reject_vals = x0
-    new_reject_funct_min = objective_to_min(new_reject_vals)
-    old_reject_func_min = -np.inf
-    old_reject_vals = new_reject_vals
-    while new_reject_funct_min > old_reject_func_min + precision:
-        old_reject_vals = new_reject_vals
-        old_reject_func_min = new_reject_funct_min
-        val_subs = dict(zip(list_of_symbols, new_reject_vals))
-        sdp.solve(feas_as_optim=True,
-                  solver_arguments={
-                      "known_vars": make_numerical(names_to_expr_dict,
-                                                   val_subs)})
-        nonneg_expr = sum(names_to_expr_dict[k] * v
-                          for k, v in
-                          sdp.solution_object["dual_certificate"].items())
-        nonneg_func = lambdify(list_of_symbols,
-                               nonneg_expr,
-                               modules='numpy', cse=True)
-        constraints = ({'type': 'ineq', 'fun': nonneg_func})
-        solution = minimize(objective_to_min, x0,
-                            method='SLSQP',
-                            bounds=bounds,
-                            constraints=constraints,
-                            options={'disp': False})
-        new_reject_vals = solution['x']
-        new_reject_funct_min = solution['fun']
-        if verbose:
-            if len(new_reject_vals) == 1:
-                current_rejection = new_reject_vals[0]
-            else:
-                current_rejection = new_reject_vals
-            print("Currently rejected value" + plural + ":", current_rejection)
-    return {"best_feasible_values": old_reject_vals,
-            "certificate": sdp.certificate_as_probs(),
-            "precision": precision}
-
-
 def max_such_that_feasible_via_dual(sdp: InflationSDP,
                                     dist_as_func: Callable,
-                                    **kwargs) -> tuple:
+                                    bounds=np.array([0.0, 1.0]),
+                                    precision=0.0001,
+                                    verbose=False) -> tuple:
     """
-    Maximizes a single symbolic variable such that the distribution (resulting
-    from applying a function to the value of the variable) makes the SDP
+    Maximizes visibility such that the distribution (resulting
+    from applying a function to the value of the visibility) makes the SDP
     feasible under set_distribution() and solve(). Internally uses certificates
-    from the dual solutions to reject as large a swath as possible. Keyword
-    arguments are passed forward to the 'symbolic_values_sdp_optimize' function.
+    from the dual solutions to reject as large a swath as possible.
 
     Parameters
     ----------
     sdp: InflationSDP
     dist_as_func: function
-        A function which, given a scalar value, return a numpy array suitable
-        for passing as an argument to sdp.set_distribution.
+        A function which, given a scalar value for the visibility, returns a
+        numpy array suitable for passing as an argument to `set_distribution`.
+    bounds: numpy.ndarray, optional
+        An array of two values, indicating respectively the upper and lower
+        numerical values to be considered for the visibility.
+        When unspecified, defaults to a numerical range between 0 and 1.
+    precision: float, optional
+        The iterative exploration of numerical values for the visibility
+        ceases if the rejected visibility range does not
+        shrink by at least this value between iterations. If unspecified, the
+        precision is set to 1e-4. May be set to zero.
+    verbose: bool, optional
+        If True, displays the updated numerical value of visibility every time
+         `solve()` is called for the InflationSDP.
 
     Returns
     -------
-    A tuple where index 0 gives the largest value that makes the SDP feasible,
-    index 1 gives the certificates as symbolic polynomial expression, and
-    index 2 gives the precision employed.
+    A tuple where index 0 gives the largest visibility value that makes the
+    SDP feasible, and index 1 gives the certificates as a symbolic polynomial
+    expression.
     """
     vis = var('v', real=True)
+    func_to_minimize = lambdify([vis], -vis, modules='numpy', cse=True)
     sdp.set_distribution(dist_as_func(vis))
-    sympolic_values = sdp._prepare_solver_arguments()["known_vars"]
-    optimality_dict = symbolic_values_sdp_optimize(
-        sdp=sdp,
-        list_of_symbols=[vis],
-        names_to_expr_dict=sympolic_values,
-        **kwargs)
-    return (optimality_dict["best_feasible_values"][0],
-            optimality_dict["certificate"], optimality_dict["precision"])
+    names_to_expr_dict = sdp._prepare_solver_arguments()["known_vars"]
+    bounds_for_scipy = Bounds(lb=bounds[0], ub=bounds[1])
+    x0 = np.array([bounds[0]])
+    new_reject_val = 1
+    old_reject_val = np.inf
+    discovered_certificates = []
+    while new_reject_val < old_reject_val - precision:
+        old_reject_val = new_reject_val
+        val_sub = {vis: old_reject_val}
+        new_known_vars = make_numerical(names_to_expr_dict, val_sub)
+        sdp.solve(feas_as_optim=True,
+                  solver_arguments={"known_vars": new_known_vars})
+        discovered_certificates.append(sdp.certificate_as_probs())
+        nonneg_expr = sum(names_to_expr_dict[k] * v
+                          for k, v in
+                          sdp.solution_object["dual_certificate"].items())
+        nonneg_func = lambdify([vis],
+                               nonneg_expr,
+                               modules='numpy', cse=True)
+
+        constraints = {'type': 'ineq', 'fun': nonneg_func}
+        solution = minimize(func_to_minimize, x0,
+                            method='SLSQP',
+                            bounds=bounds_for_scipy,
+                            constraints=constraints,
+                            options={'disp': False})
+        new_reject_val = solution['x'][0]
+        if verbose:
+            print("Currently rejected value:", new_reject_val)
+    return min(new_reject_val, old_reject_val), discovered_certificates[-1]
 
 
 def bisect(f: Callable,
@@ -169,8 +102,8 @@ def bisect(f: Callable,
     Parameters
     ----------
     f: function
-        A function of a single variable whose negativity indicates the rejection
-        of the input value
+        A function of a single variable whose negativity indicates the
+        rejection of the input value
     bounds: tuple, optional
         A specification of the range to explore for the target variable.
     precision: float, optional
