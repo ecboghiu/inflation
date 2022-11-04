8e5239fb2839fb5a0659f7be0f5a8e7129e493f1
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2022-11-04 11:00:55 +0100

    Unify functions
    Now the user shall just call `max_within_feasible`, specifying the desired method.

--- inflation/quantum/optimization_utils.py
+++ inflation/quantum/optimization_utils.py
@@ -6,13 +6,15 @@ distribution for which quantum inflation (at specific hierarchy levels) is
 @authors: Alejandro Pozas-Kerstjens, Elie Wolfe, Emanuel-Cristian Boghiu
 """
 import numpy as np
+import sympy as sp
 
 from scipy.optimize import minimize, Bounds
 from sympy import Float
 from sympy.utilities.lambdify import lambdify
-from typing import Callable, Dict
+from typing import Callable, Dict, Tuple, Union
 
 from causalinflation import InflationSDP
+from causalinflation.quantum.general_tools import make_numerical
 from causalinflation.quantum.monomial_classes import CompoundMonomial
 
 
@@ -20,7 +22,8 @@ def bisect(f: Callable,
            bounds=(0.0, 1.0),
            precision=1e-4,
            verbose=False,
-           tolerance=0) -> float:
+           tolerance=0,
+           **kwargs) -> float:
     """Applies a bisection algorithm in order to find the largest value of a
     variable --- within some bounds --- which makes a function of the variable
     nonnegative. Its intended use is to maximize VISIBILITY, such that the
@@ -72,10 +75,35 @@ def bisect(f: Callable,
     return x
 
 
-def max_within_feasible_via_bisect(sdp: InflationSDP,
-                                   symbolic_values: Dict[CompoundMonomial,
-                                                         Callable],
-                                   **kwargs) -> float:
+def max_within_feasible(sdp: InflationSDP,
+                        symbolic_values: Dict[CompoundMonomial, Callable],
+                        method: str,
+                        return_last_certificate=False,
+                        **kwargs):
+    """Docs
+    """
+    assert method in ["bisection", "dual"], \
+        "Unknown optimization method. Please use \"bisection\" or \"dual\"."
+    variables = set()
+    for expr in symbolic_values.values():
+        if type(expr) not in [Float, float]:
+            variables.update(expr.free_symbols)
+    assert len(variables) == 1, \
+        "Only optimization of a single variable is supported"
+    param = variables.pop()
+    kwargs.update({"return_last_certificate": return_last_certificate})
+
+    if method == "bisection":
+        return _maximize_via_bisect(sdp, symbolic_values, param, **kwargs)
+    elif method == "dual":
+        return _maximize_via_dual(sdp, symbolic_values, param, **kwargs)
+
+
+def _maximize_via_bisect(sdp: InflationSDP,
+                         symbolic_values: Dict[CompoundMonomial, Callable],
+                         param: sp.core.symbol.Symbol,
+                         **kwargs) -> Union[float,
+                                            Tuple[float, sp.core.add.Add]]:
     """Maximizes a single symbolic variable such that the distribution (resulting
     from applying a function to the value of the variable) makes the SDP
     feasible under set_distribution() and solve(). Internally uses a bisection
@@ -95,37 +123,27 @@ def max_within_feasible_via_bisect(sdp: InflationSDP,
     The largest value (within the given range) which makes the SDP feasible, or
     negative infinity if the SDP cannot be made feasible in that range.
     """
-    variables = set()
-    for expr in symbolic_values.values():
-        if type(expr) not in [Float, float]:
-            variables.update(expr.free_symbols)
-
-    assert len(variables) == 1, \
-        "Only optimization of a single variable is supported"
-    use_lpi = kwargs.get("use_lpi_constraints", False)
     only_specified = kwargs.get("only_specified_values", False)
-    param = variables.pop()
+    return_last    = kwargs.get("return_last_certificate", False)
+    use_lpi        = kwargs.get("use_lpi_constraints", False)
 
     def f(value):
-        evaluated_values = {}
-        for mon, expr in symbolic_values.items():
-            if type(expr) not in [Float, float]:
-                evaluated_values[mon] = float(expr.subs(param, value))
-            else:
-                evaluated_values[mon] = float(expr)
+        evaluated_values = make_numerical(symbolic_values, {param: value})
         sdp.set_values(evaluated_values, use_lpi, only_specified)
         sdp.solve(feas_as_optim=True)
         return sdp.objective_value
-    return bisect(f=f, **kwargs)
+    crit_param = bisect(f=f, **kwargs)
+    if return_last:
+        return crit_param, sdp.certificate_as_probs()
+    else:
+        return crit_param
 
 
-def max_within_feasible_via_dual(sdp: InflationSDP,
-                                 symbolic_values: Dict[CompoundMonomial,
-                                                       Callable],
-                                 bounds=np.array([0.0, 1.0]),
-                                 precision=0.0001,
-                                 verbose=False,
-                                 **kwargs) -> tuple:
+def _maximize_via_dual(sdp: InflationSDP,
+                       symbolic_values: Dict[CompoundMonomial, Callable],
+                       param: sp.core.symbol.Symbol,
+                       **kwargs) -> Union[float,
+                                          Tuple[float, sp.core.add.Add]]:
     """Maximizes visibility such that the distribution (resulting
     from applying a function to the value of the visibility) makes the SDP
     feasible under set_distribution() and solve(). Internally uses certificates
@@ -156,32 +174,25 @@ def max_within_feasible_via_dual(sdp: InflationSDP,
     SDP feasible, and index 1 gives the certificates as a symbolic polynomial
     expression.
     """
-    variables = set()
-    for expr in symbolic_values.values():
-        if type(expr) not in [Float, float]:
-            variables.update(expr.free_symbols)
-    assert len(variables) == 1, \
-        "Only optimization of a single variable is supported"
-    use_lpi = kwargs.get("use_lpi_constraints", False)
+    bounds         = kwargs.get("bounds", np.array([0.0, 1.0]))
     only_specified = kwargs.get("only_specified_values", False)
+    precision      = kwargs.get("precision", 1e-4)
+    return_last    = kwargs.get("return_last_certificate", False)
+    use_lpi        = kwargs.get("use_lpi_constraints", False)
+    verbose        = kwargs.get("verbose", False)
+
     symbol_names = {str(key): val for key, val in symbolic_values.items()}
-    param = variables.pop()
     func_to_minimize = lambdify([param], -param, modules='numpy', cse=True)
     sp_bounds = Bounds(lb=bounds[0], ub=bounds[1])
     x0 = np.array([bounds[0]])
-    new_reject_val = bounds[1]
-    old_reject_val = np.inf
+    new_ub = bounds[1]
+    old_ub = np.inf
     discovered_certificates = []
     # Get a certificate for a value, find the critical value for it (that which
     # makes the certificate zero), and repeat with this new value
-    while new_reject_val < old_reject_val - precision:
-        old_reject_val = new_reject_val
-        evaluated_values = {}
-        for mon, expr in symbolic_values.items():
-            if type(expr) not in [Float, float]:
-                evaluated_values[mon] = float(expr.subs(param, old_reject_val))
-            else:
-                evaluated_values[mon] = float(expr)
+    while new_ub < old_ub - precision:
+        old_ub = new_ub
+        evaluated_values = make_numerical(symbolic_values, {param: new_ub})
         sdp.set_values(evaluated_values, use_lpi, only_specified)
         sdp.solve(feas_as_optim=True)
         discovered_certificates.append(sdp.certificate_as_probs())
@@ -199,7 +210,11 @@ def max_within_feasible_via_dual(sdp: InflationSDP,
                             bounds=sp_bounds,
                             constraints=constraints,
                             options={'disp': False})
-        new_reject_val = solution['x'][0]
+        new_ub = solution['x'][0]
         if verbose:
-            print("Currently rejected value:", new_reject_val)
-    return min(new_reject_val, old_reject_val), discovered_certificates[-1]
+            print("Current critical value:", new_ub)
+    crit_param = min(new_ub, old_ub)
+    if return_last:
+        return crit_param, discovered_certificates[-1]
+    else:
+        return crit_param
