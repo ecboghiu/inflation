bdf0d656be349522984faf086e24115c1c198061
Erica Han <ehan@perimeterinstitute.ca>
2023-06-13 16:03:55 -0400

    Added LP write_to_mps functionality (which does not use Mosek), plus LP writer tests

--- inflation/lp/writer_utils.py
+++ inflation/lp/writer_utils.py
@@ -1,8 +1,8 @@
 import mosek
+from copy import deepcopy
 
 
-def write_to_lp(args: dict,
-                filename: str) -> None:
+def write_to_lp(args: dict, filename: str) -> None:
     """Export the problem to a file in .lp format. Specification can be found
     at https://docs.mosek.com/latest/pythonapi/lp-format.html.
 
@@ -13,13 +13,192 @@ def write_to_lp(args: dict,
     filename : str
         The file to write to.
     """
-    objective = args['objective']
-    known_vars = args['known_vars']
-    semiknown_vars = args['semiknown_vars']
-    equalities = args['equalities']
-    inequalities = args['inequalities']
-    lower_bounds = args['lower_bounds']
-    upper_bounds = args['upper_bounds']
+    (objective,
+     known_vars,
+     semiknown_vars,
+     internal_equalities,
+     inequalities,
+     lower_bounds,
+     upper_bounds,
+     variables) = process_arguments(args)
+
+    # Write problem to file
+    f = open(filename, "w")
+    f.write(f"\\ File: {filename}\n")
+    f.write("maximize\n")
+    f.write(f"obj: {format_constraint_lp(known_vars, objective, 'obj')}\n")
+    f.write("subject to\n")
+
+    i = 0
+    for eq in internal_equalities:
+        cons = format_constraint_lp(known_vars, eq, 'eq')
+        if cons != "":
+            f.write(f"c{i}: {cons}\n")
+            i += 1
+    for ineq in inequalities:
+        cons = format_constraint_lp(known_vars, ineq, 'ineq')
+        if cons != "":
+            f.write(f"c{i}: {cons}\n")
+            i += 1
+
+    f.write("bounds\n")
+    for var in variables:
+        if var in lower_bounds:
+            lb = lower_bounds[var]
+        else:
+            lb = "-infinity"
+        if var in upper_bounds:
+            ub = upper_bounds[var]
+        else:
+            ub = "+infinity"
+        f.write(f" {lb} <= {var} <= {ub}\n")
+    f.write("end")
+    f.close()
+
+
+def write_to_mps(args: dict, filename: str) -> None:
+    """Export the problem to a file in .lp format. Specification can be found
+    at https://docs.mosek.com/latest/pythonapi/lp-format.html.
+
+    Parameters
+    ----------
+    args : dict
+        The arguments of the problem to write to the file.
+    filename : str
+        The file to write to.
+    """
+    (objective,
+     known_vars,
+     semiknown_vars,
+     internal_equalities,
+     inequalities,
+     lower_bounds,
+     upper_bounds,
+     variables) = process_arguments(args)
+
+    # Exclude constraints in which variables are all known values
+    internal_equalities = [eq for eq in internal_equalities
+                           if set(eq).difference(known_vars)]
+    inequalities = [ineq for ineq in inequalities
+                    if set(ineq).difference(known_vars)]
+    constraints = internal_equalities + inequalities
+    nof_equalities = len(internal_equalities)
+    nof_inequalities = len(inequalities)
+
+    f = open(filename, "w")
+    f.write(f"* File: {filename}\n")
+    f.write(f"NAME          {filename.split('.')[0]}\n")
+    f.write(f"OBJSENSE\n    MAX\n")
+
+    # Specify rows of the problem
+    f.write(f"ROWS\n")
+    f.write(f" N  obj\n")
+    for i in range(nof_equalities):
+        f.write(f" E  c{i}\n")
+    for i in range(nof_inequalities):
+        f.write(f" G  c{nof_equalities + i}\n")
+
+    # Specify columns of the problem
+    f.write(f"COLUMNS\n")
+    for x in variables:
+        if x in set(objective).difference(known_vars):
+            arr = [x, 'obj', objective[x]]
+            f.write("    {:10}{:10}{:<10.4f}\n".format(*arr))
+        for i, eq in enumerate(internal_equalities):
+            if x in set(eq).difference(known_vars):
+                arr = [x, f"c{i}", eq[x]]
+                f.write("    {:10}{:10}{:<10.4f}\n".format(*arr))
+        for i, ineq in enumerate(inequalities):
+            if x in set(ineq).difference(known_vars):
+                arr = [x, f"c{nof_equalities + i}",
+                       ineq[x]]
+                f.write("    {:10}{:10}{:<10.4f}\n".format(*arr))
+
+    f.write(f"RHS\n")
+    # Specify objective offset
+    rhs = 0
+    for x in set(objective).intersection(known_vars):
+        rhs -= objective[x] * known_vars[x]
+    if rhs != 0:
+        arr = ['rhs', 'obj', rhs]
+        f.write("    {:10}{:10}{:<10.4f}\n".format(*arr))
+
+    # Specify non-zero RHS values of the constraints
+    for i, c in enumerate(constraints):
+        rhs = 0
+        for x in set(c).intersection(known_vars):
+            rhs -= c[x] * known_vars[x]
+        if rhs != 0:
+            arr = ['rhs', f"c{i}", rhs]
+            f.write("    {:10}{:10}{:<10.4f}\n".format(*arr))
+
+    f.write(f"RANGES\n")
+
+    # Specify variable bound values
+    f.write(f"BOUNDS\n")
+    for x in lower_bounds:
+        arr = ['bound', x, lower_bounds[x]]
+        f.write(" LO {:10}{:10}{:<10.4f}\n".format(*arr))
+    for x in upper_bounds:
+        arr = ['bound', x, upper_bounds[x]]
+        f.write(" UP {:10}{:10}{:<10.4f}\n".format(*arr))
+    f.write(f"ENDATA")
+    f.close()
+
+
+def lp_to_mps(args: dict, filename: str) -> None:
+    """Export the problem to a file in .mps format by writing it as an LP file
+    then converting it to MPS through Mosek.
+
+    Parameters
+    ----------
+    args : dict
+        The arguments of the problem to write to the file.
+    filename : str
+        The file to write to.
+    """
+    lp_filename = f"{filename.split('.')[0]}.lp"
+    write_to_lp(args, lp_filename)
+    with mosek.Task() as t:
+        t.readdata(f"{lp_filename}")
+        t.writedata(filename)
+
+
+def process_arguments(args: dict) -> tuple:
+    """Helper function that processes arguments and variables in preparation
+    for writing.
+
+    Parameters
+    ----------
+    args : dict
+        The arguments to be processed.
+
+    Returns
+    -------
+    tuple
+        Processed objective, known_vars, semiknown_vars, internal_equalities,
+        inequalities, lower_bounds, upper_bounds, variables
+    """
+    args = deepcopy(args)
+    objective = args.get('objective')
+    known_vars = args.get('known_vars')
+    if known_vars is None:
+        known_vars = {}
+    semiknown_vars = args.get('semiknown_vars')
+    if semiknown_vars is None:
+        semiknown_vars = {}
+    equalities = args.get('equalities')
+    if equalities is None:
+        equalities = []
+    inequalities = args.get('inequalities')
+    if inequalities is None:
+        inequalities = []
+    lower_bounds = args.get('lower_bounds')
+    if lower_bounds is None:
+        lower_bounds = {}
+    upper_bounds = args.get('upper_bounds')
+    if upper_bounds is None:
+        upper_bounds = {}
 
     # Process semiknown variables
     internal_equalities = equalities.copy()
@@ -49,59 +228,10 @@ def write_to_lp(args: dict,
         lower_bounds[var_index[x]] = lower_bounds.pop(x)
     for x in set(upper_bounds).difference(known_vars):
         upper_bounds[var_index[x]] = upper_bounds.pop(x)
-    variables = set(var_index.values())
+    variables = list(var_index.values())
 
-    # Write problem to file
-    f = open(filename, "w")
-    f.write(f"\\ File: {filename}\n")
-    f.write("maximize\n")
-    f.write(f"obj: {format_constraint_lp(known_vars, objective, 'obj')}\n")
-    f.write("subject to\n")
-
-    i = 1
-    for eq in internal_equalities:
-        cons = format_constraint_lp(known_vars, eq, 'eq')
-        if cons != "":
-            f.write(f"c{i}: {cons}\n")
-            i += 1
-    for ineq in inequalities:
-        cons = format_constraint_lp(known_vars, ineq, 'ineq')
-        if cons != "":
-            f.write(f"c{i}: {cons}\n")
-            i += 1
-
-    f.write("bounds\n")
-    for var in variables:
-        if var in lower_bounds:
-            lb = lower_bounds[var]
-        else:
-            lb = "-infinity"
-        if var in upper_bounds:
-            ub = upper_bounds[var]
-        else:
-            ub = "+infinity"
-        f.write(f" {lb} <= {var} <= {ub}\n")
-    f.write("end")
-    f.close()
-
-
-def write_to_mps(args: dict,
-                 filename: str) -> None:
-    """Export the problem to a file in .mps format. Specification can be found
-    at https://docs.mosek.com/latest/pythonapi/mps-format.html.
-
-    Parameters
-    ----------
-    args : dict
-        The arguments of the problem to write to the file.
-    filename : str
-        The file to write to.
-    """
-    lps_filename = f"{filename.split('.')[0]}.lp"
-    write_to_lp(args, lps_filename)
-    with mosek.Task() as t:
-        t.readdata(f"{lps_filename}")
-        t.writedata(filename)
+    return objective, known_vars, semiknown_vars, internal_equalities, \
+        inequalities, lower_bounds, upper_bounds, variables
 
 
 def format_constraint_lp(known_vars: dict,
---
+++ test/test_writers.py
@@ -0,0 +1,50 @@
+import unittest
+import warnings
+import mosek
+import numpy as np
+
+from inflation import InflationProblem, InflationLP
+from inflation.lp.writer_utils import write_to_lp, write_to_mps
+
+
+class TestWriters(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+    p = np.zeros((2, 2, 2, 1))
+    p[0, 0, 0, 0] = 0.3
+    p[1, 0, 0, 0] = 0.7
+    p[0, 0, 1, 0] = 0.7
+    p[1, 1, 1, 0] = 0.3
+    p = 0.9 * p + 0.1 * (1 / 4)
+
+    instrumental = InflationProblem({"U_AB": ["A", "B"],
+                                     "A": ["B"]},
+                                    outcomes_per_party=(2, 2),
+                                    settings_per_party=(2, 1),
+                                    inflation_level_per_source=(1,),
+                                    order=("A", "B"),
+                                    classical_sources=["U_AB"])
+    instrumental_infLP = InflationLP(instrumental,
+                                     nonfanout=False,
+                                     verbose=False)
+    instrumental_infLP.set_distribution(p)
+    instrumental_infLP.set_objective(objective={'<B_1_0_0>': 1},
+                                     direction='max')
+    instrumental_infLP.solve()
+    args = instrumental_infLP._prepare_solver_arguments(separate_bounds=True)
+    primal_value = instrumental_infLP.objective_value
+
+    def test_writers(self):
+        for (write, ext) in ((write_to_lp, 'lp'), (write_to_mps, 'mps')):
+            with self.subTest():
+                write(self.args, f"inst.{ext}")
+                with mosek.Task() as task:
+                    task.readdata(f"inst.{ext}")
+                    task.optimize()
+                    obj = task.getprimalobj(mosek.soltype.bas)
+                self.assertAlmostEqual(self.primal_value, obj,
+                                       msg=f"The expected value and value when"
+                                           f" reading from {ext.upper()} are "
+                                           f"not equal.")
