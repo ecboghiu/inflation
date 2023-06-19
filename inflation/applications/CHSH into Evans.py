b32a87dc1d971b39b6692d08a6cea0fa9875abee
Elie Wolfe <elie@causarum.org>
2023-06-19 16:55:21 -0400

    Updating CHSH into Evans example

--- inflation/applications/CHSH into Evans.py
+++ inflation/applications/CHSH into Evans.py
@@ -4,14 +4,16 @@ import numpy as np
 p_Q = np.zeros((2, 2, 2, 1, 1, 1), dtype=float)
 q = [1 / 2, 1 / 2]
 p_Q_do = np.zeros((2, 2, 2), dtype=float)
-v = 1 / np.sqrt(2)
+# v = 1 / np.sqrt(2)
+# v = 0.72
+v = 1
 
 for a in range(2):
     for b in range(2):
         for c in range(2):
             p_Q_do[a, b, c] = q[c] * (1 / 2)
             p_Q[a, b, c] = (q[c]) * (1 / 4) * (
-            (1 + (v) * ((-1) ** (a + b + b * c))))
+                (1 + (v) * ((-1) ** (a + b + b * c))))
 
 Evans = InflationProblem({"U_AB": ["A", "B"],
                           "U_BC": ["B", "C"],
@@ -24,31 +26,18 @@ Evans = InflationProblem({"U_AB": ["A", "B"],
 Evans_Unpacked = InflationLP(Evans,
                              nonfanout=False,
                              verbose=2)
-Evans_Unpacked.set_distribution(p_Q)
-known_do_values = {f'<C_0_1_{b}_{c}>': q[c]
-                   for c in range(1)
-                   for b in range(2)}
-# known_do_values.update({f'<C_0_2_{b}_{c}>': q[c]
-#                         for c in range(1)
-#                         for b in range(2)})
-# known_do_values.update({f'<A_1_0_{b}_{a}>': 1 / 2
-#                         for a in range(1)
-#                         for b in range(2)})
-known_do_values.update({f'<A_2_0_{b}_{a}>': 1 / 2
-                        for a in range(1)
-                        for b in range(2)})
-# known_do_values.update({f'<A_1_0_{b1}_{a}>*<C_0_1_{b2}_{c}>': q[c]*1 / 2
-#                         for a in range(2)
-#                         for c in range(2)
-#                         for b1 in range(2)
-#                         for b2 in range(2)})
-Evans_Unpacked.set_values(known_do_values,
-                          use_lpi_constraints=False)
+semiknown_usage = False
+Evans_Unpacked.set_distribution(p_Q, use_lpi_constraints=semiknown_usage)
+Evans_Unpacked.set_values({
+    '<A_1_0_0_0>': 1 / 2,
+    '<A_1_0_1_0>': 1 / 2
+},
+    use_lpi_constraints=semiknown_usage)
+
 print(f"Known Values: {Evans_Unpacked.known_moments}")
 Evans_Unpacked.solve(dualise=False, verbose=2)
-print(Evans_Unpacked.status)
-x_dict = {n: np.round(v, decimals=3) for n, v in Evans_Unpacked.solution_object['x'].items() if np.abs(v)>1e-10}
+print("Status: ", Evans_Unpacked.status)
+x_dict = {n: np.round(v, decimals=5) for n, v in
+          Evans_Unpacked.solution_object['x'].items() if np.abs(v) > 1e-5}
 print(x_dict)
 print(Evans_Unpacked.certificate_as_probs())
-
-
