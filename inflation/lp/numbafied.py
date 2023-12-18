06da0197311d8c593b1e3c701b7a2a6a5a115419
Elie Wolfe <elie@causarum.org>
2023-12-18 12:07:04 -0500

    Numbification of apply_lexorder_perm_to_lexboolvecs (used in LP). Possible concern around integer overflow for InflationLPs with lexorder exceeding 64 operators, but all tests pass.

--- inflation/lp/numbafied.py
+++ inflation/lp/numbafied.py
@@ -8,6 +8,8 @@ import numpy as np
 try:
     from numba import jit, prange, bool_
     from numba.types import int_
+    from numba.typed import Dict, List, Set
+    empty_dict = Dict.empty(key_type=int_, value_type=int_)
     nopython = True
 except ImportError:
     def jit(*args, **kwargs):
@@ -16,7 +18,9 @@ except ImportError:
     int_     = int
     nopython = False
     prange   = range
-
+    empty_dict = dict()
+    List = list
+    Set = set
 cache    = True
 if not nopython:
     bool_  = bool
@@ -52,23 +56,36 @@ def nb_mon_to_lexrepr_bool(mon: np.ndarray,
                 break
     return in_lex
 
-# @jit(nopython=nopython, cache=cache, forceobj=not nopython)
+@jit(nopython=nopython, cache=cache, forceobj=not nopython)
+def bitvec_to_int(bitvec: np.ndarray) -> int_:
+    val = 0
+    for b in bitvec.flat:
+        val = np.left_shift(val, 1)
+        val = np.bitwise_or(val, b)
+    return val
+
+
+@jit(nopython=nopython, cache=cache, forceobj=not nopython)
 def nb_apply_lexorder_perm_to_lexboolvecs(monomials_as_lexboolvecs: np.ndarray,
                                           lexorder_perms: np.ndarray) -> np.ndarray:
     # Note: dictionary creation seems wasteful,
     # but this function is not called within loops.
-    lookup_dict = {bitvec.tobytes(): i for i, bitvec in
-                   enumerate(monomials_as_lexboolvecs)}
-    orbits = np.zeros(len(monomials_as_lexboolvecs), dtype=int) - 1
+    lookup_dict = empty_dict.copy()
+    for i, bitvec in enumerate(monomials_as_lexboolvecs):
+        lookup_dict[bitvec_to_int(bitvec)] = i
+    lookup_dict_keys = Set(lookup_dict.keys())
+    # lookup_dict = {bitvec_to_int(bitvec): i for i, bitvec in
+    #                enumerate(monomials_as_lexboolvecs)}
+    orbits = np.zeros(len(monomials_as_lexboolvecs), dtype=int_) - 1
     for i, default_lexboolvec in enumerate(monomials_as_lexboolvecs):
         if orbits[i] == -1:
-            alternative_lexboolvecs = default_lexboolvec[lexorder_perms]
-            equivalent_monomial_positions = list()
-            for bitvec in alternative_lexboolvecs:
-                try:
-                    equivalent_monomial_positions.append(lookup_dict[bitvec.tobytes()])
-                except KeyError:
-                    pass
+            # alternative_lexboolvecs = default_lexboolvec[lexorder_perms]
+            equivalent_monomial_positions = List()
+            for perm in lexorder_perms:
+                bitvec = default_lexboolvec[perm]
+                bitvec_as_int = bitvec_to_int(bitvec)
+                if bitvec_as_int in lookup_dict_keys:
+                    equivalent_monomial_positions.append(lookup_dict[bitvec_as_int])
             orbits[equivalent_monomial_positions] = i
     return orbits
 
