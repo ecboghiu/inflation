204fd59d1f92c42dc2da68d2b9ac618935868662
Alex Pozas-Kerstjens <apozas@users.noreply.github.com>
2024-03-05 01:58:51 +0100

    Add tests on notcomm matrix
    One test is failing, but I think it highlights a bug in the code.

--- test/test_monomial_properties.py
+++ test/test_monomial_properties.py
@@ -191,3 +191,113 @@ class TestToRepr(unittest.TestCase):
         self.assertEqual(self.sdp_noncommuting._sanitise_moment(initial),
                          self.sdp_noncommuting._sanitise_moment(correct),
                          "Swapping a single source fails.")
+
+class TestCommutations(unittest.TestCase):
+    scenario = InflationProblem(dag={"s1": ["A", "B", "l1", "l2"],
+                                     "s2": ["C", "D"],
+                                     "s3": ["l2"],
+                                     "l1": ["C", "D"],
+                                     "l2": ["B", "E"]},
+                                outcomes_per_party=[2, 2, 2, 2, 2],
+                                settings_per_party=[2, 2, 2, 2, 2],
+                                inflation_level_per_source=[2, 2, 2],
+                                classical_sources=["s2"],
+                                nonclassical_intermediate_latents=["l1"],
+                                classical_intermediate_latents=["l2"])
+    notcomm = scenario._default_notcomm
+    meas    = scenario.measurements
+    order   = scenario._lexorder
+    
+    def test_commute_itself(self):
+        # All operators should commute with themselves.
+        self.assertEqual(self.notcomm.diagonal().sum(),
+                         0,
+                         "Some operator does not commute with itself.")
+    
+    def test_same_nonclassical(self):
+        # The operators corresponding to different measurements of a same party
+        # fed by nonclassical sources do not commute. Take for example A.
+        op1_init = np.where(np.all(self.order == self.meas[0][0,0,0], axis=1))[0]
+        op1_end  = np.where(np.all(self.order == self.meas[0][0,0,-1], axis=1))[0]
+        op2_init = np.where(np.all(self.order == self.meas[0][0,-1,0], axis=1))[0]
+        op2_end  = np.where(np.all(self.order == self.meas[0][0,-1,-1], axis=1))[0]
+        op1_init, op1_end = op1_init.item(), op1_end.item()
+        op2_init, op2_end = op2_init.item(), op2_end.item()
+        self.assertTrue(
+            np.all(self.notcomm[op1_init:op1_end+1, op2_init:op2_end+1]),
+            "Operators for different measurement of a same party commute.")
+        
+    def test_same_classical(self):
+        # All operators corresponding to a same party fed by classical sources
+        # commute. Take for example E.
+        op1_init = np.where(np.all(self.order == self.meas[-1][0,0,0], axis=1))[0]
+        op1_end  = np.where(np.all(self.order == self.meas[-1][-1,-1,-1], axis=1))[0]
+        op1_init, op1_end = op1_init.item(), op1_end.item()
+        self.assertEqual(
+            np.sum(self.notcomm[op1_init:op1_end+1, op1_init:op1_end+1]),
+            0,
+            "Operators of a classical party do not commute.")
+
+    def test_source_parties(self):
+        # We restrict to the case of parties connected (or not) by a source.
+        # Regardless of the nature of the sources, they should commute. This
+        # applies to all pairs of parties, except BE (connected by a classical
+        # latent, tretated later), and CD (connected by a quantum latent and a
+        # classical source, so they only commute if the nonclassical copies do
+        # not overlap, treated later)
+        for pair in [[0, 1], [0, 2], [0, 3], [0, 4],
+                     [1, 2], [1, 3], [2, 4], [3, 4]]:
+            op1_init = np.where(np.all(self.order == self.meas[pair[0]][0,0,0], axis=1))[0]
+            op1_end  = np.where(np.all(self.order == self.meas[pair[0]][-1,-1,-1], axis=1))[0]
+            op2_init = np.where(np.all(self.order == self.meas[pair[1]][0,0,0], axis=1))[0]
+            op2_end  = np.where(np.all(self.order == self.meas[pair[1]][-1,-1,-1], axis=1))[0]
+            op1_init, op1_end = op1_init.item(), op1_end.item()
+            op2_init, op2_end = op2_init.item(), op2_end.item()
+            self.assertEqual(self.notcomm[op1_init:op1_end+1, op2_init:op2_end+1].sum(),
+                             0,
+                             "Operators for separate parties " + \
+                             f"{chr(65+pair[0])} and {chr(65+pair[1])} do not commute.")
+    
+    def test_classical_nonclassical_sources(self):
+        # The CD case. Here we only have commutation if the nonclassical copies
+        # are disjoint
+        op1_init = np.where(np.all(self.order == self.meas[2][0,0,0], axis=1))[0]
+        op1_end  = np.where(np.all(self.order == self.meas[2][3,-1,-1], axis=1))[0]
+        op2_init = np.where(np.all(self.order == self.meas[3][-4,0,0], axis=1))[0]
+        op2_end  = np.where(np.all(self.order == self.meas[3][-1,-1,-1], axis=1))[0]
+        op1_init, op1_end = op1_init.item(), op1_end.item()
+        op2_init, op2_end = op2_init.item(), op2_end.item()
+        self.assertEqual(self.notcomm[op1_init:op1_end+1, op2_init:op2_end+1].sum(),
+                         0,
+                         "Operators for separate parties with classical and " \
+                         + " nonclassical parents do not commute when " \
+                         + "measuring on different nonclassical copies.")
+
+    def test_nonclassical_latent(self):
+        # When two parties share a common nonclassical latent ancestor, their
+        # operators over the exact same copies do not commute.
+        op1_init = np.where(np.all(self.order == self.meas[2][0,0,0], axis=1))[0]
+        op1_end  = np.where(np.all(self.order == self.meas[2][0,-1,-1], axis=1))[0]
+        op2_init = np.where(np.all(self.order == self.meas[3][0,0,0], axis=1))[0]
+        op2_end  = np.where(np.all(self.order == self.meas[3][0,-1,-1], axis=1))[0]
+        op1_init, op1_end = op1_init.item(), op1_end.item()
+        op2_init, op2_end = op2_init.item(), op2_end.item()
+        self.assertTrue(
+            np.all(self.notcomm[op1_init:op1_end+1, op2_init:op2_end+1]),
+            "Operators for parties with a common nonclassical latent " + \
+            "ancestor commute for same nonclassical copies.")
+
+    def test_classical_latent(self):
+        # When two parties share a common classical latent ancestor, their
+        # operators should always commute. This is the case for BE.
+        op1_init = np.where(np.all(self.order == self.meas[1][0,0,0], axis=1))[0]
+        op1_end  = np.where(np.all(self.order == self.meas[1][-1,-1,-1], axis=1))[0]
+        op2_init = np.where(np.all(self.order == self.meas[4][0,0,0], axis=1))[0]
+        op2_end  = np.where(np.all(self.order == self.meas[4][-1,-1,-1], axis=1))[0]
+        op1_init, op1_end = op1_init.item(), op1_end.item()
+        op2_init, op2_end = op2_init.item(), op2_end.item()
+        self.assertEqual(
+            np.sum(self.notcomm[op1_init:op1_end+1, op2_init:op2_end+1]),
+            0,
+            "Operators for parties with a common classical latent ancestor " + \
+            "do not commute.")
