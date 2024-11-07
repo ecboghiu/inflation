a7d70986479a5919fbdee21e8001e0038710b8a7
Elie Wolfe <elupus@gmail.com>
2024-11-07 12:22:47 -0500

    Demonstration of no 3-coloring of triangle under party-relabelling and outcome-relabelling symmetry assumptions (which essentially define a unique nonclassical distribution)

--- inflation/applications/triangle_with_C3_sym.py
+++ inflation/applications/triangle_with_C3_sym.py
@@ -18,6 +18,7 @@ permuting_copy_indices[permuting_copy_indices[:,3]==0,0] = 2
 permuting_copy_indices[permuting_copy_indices[:,1]==0,0] = 3
 new_lexorder_permutation_sources = np.array([triangle._lexorder_lookup[op.tobytes()] for op in permuting_copy_indices])
 
+
 permuting_outcome_labels = triangle._lexorder.copy()
 for i in range(d):
     permuting_outcome_labels[triangle._lexorder[:,-1] == i,-1] = np.mod(i+1,d)
@@ -33,27 +34,19 @@ from sympy.combinatorics import Permutation, PermutationGroup
 group_generators = [Permutation(perm) for perm in triangle.lexorder_symmetries]
 group_generators.extend([Permutation(perm) for perm in [new_lexorder_permutation_sources,
                                                         new_lexorder_cycle_outcomes,
-                                                        new_lexorder_swap_outcomes]])
+                                                        new_lexorder_swap_outcomes
+                                                        ]])
 
 all_symmetries_new = np.array(list(PermutationGroup(group_generators).generate_schreier_sims(af=True)))
 print("Group order: ", len(all_symmetries_new))
 
-# all_symmetries_new = np.vstack((triangle.lexorder_symmetries,
-#                                 triangle.lexorder_symmetries[:,new_lexorder_permutation],
-#                                 triangle.lexorder_symmetries[:,new_lexorder_permutation[new_lexorder_permutation]]
-#                                 ))
-# all_symmetries_new_v2 = np.vstack((triangle.lexorder_symmetries,
-#                                 new_lexorder_permutation[triangle.lexorder_symmetries],
-#                                 new_lexorder_permutation[new_lexorder_permutation][triangle.lexorder_symmetries]
-#                                 ))
 triangle.lexorder_symmetries = np.unique(all_symmetries_new,axis=0)
-# assert np.array_equal(np.unique(all_symmetries_new,axis=0), np.unique(all_symmetries_new_v2,axis=0)), "Implementation matters?!"
-assert len(triangle.lexorder_symmetries)==len(all_symmetries_new), "Somehow a generator showed up twice!"
-print(triangle.lexorder_symmetries)
-identity_sym = triangle.lexorder_symmetries[0]
-for sym in triangle.lexorder_symmetries:
-    assert np.array_equal(np.unique(sym), identity_sym), "Symmetry is not a permutation!"
-# print(triangle._lexrepr_to_names)
+# assert len(triangle.lexorder_symmetries)==len(all_symmetries_new), "Somehow a generator showed up twice!"
+# print(triangle.lexorder_symmetries)
+# identity_sym = triangle.lexorder_symmetries[0]
+# for sym in triangle.lexorder_symmetries:
+#     assert np.array_equal(np.unique(sym), identity_sym), "Symmetry is not a permutation!"
+
 
 import itertools
 victors_distribution = np.zeros(shape=(d,d,d,1,1,1), dtype=float)
@@ -65,16 +58,61 @@ for a in range(d):
             for indices in indices_choices:
                 victors_distribution[indices] = val
     victors_distribution[a,a,a] = 1/d*1/d
-print(np.sum(victors_distribution))
-print(len(np.flatnonzero(victors_distribution)))
+# print(np.sum(victors_distribution))
+# print(len(np.flatnonzero(victors_distribution)))
 
-# triangleLP = InflationLP(triangle, verbose=2)
+triangleLP = InflationLP(triangle, verbose=2, supports_problem=True)
 # triangleLP.set_distribution(victors_distribution, use_lpi_constraints=True)
-# triangleLP.solve()
-# print(triangleLP.status)
+for monomial in triangleLP.atomic_monomials:
+    if monomial.n_operators < 3:
+        print(monomial)
 
-triangleSDP = InflationSDP(triangle, verbose=2)
-triangleSDP.generate_relaxation("local1")
-triangleSDP.set_distribution(victors_distribution, use_lpi_constraints=True)
-triangleSDP.solve()
-print(triangleSDP.status)
+triangleLP.set_bounds({
+    "P[A=0]": 1,
+    # "P[A=1]": 1,
+    # "P[A=2]": 1
+}, bound_type="lo")
+triangleLP.update_values({
+    "P[A=0 B=0]": 0,
+    # "P[A=1 B=1]": 0,
+    # "P[A=2 B=2]": 0
+}, use_lpi_constraints=True)
+triangleLP.solve()
+print(triangleLP.status)
+solution_x = triangleLP.solution_object["x"]
+print("These should be zero:")
+print(solution_x["P[A=0 B=0]"])
+print(solution_x["P[A=0 B=0 C=0]"])
+print(solution_x["P[A=0 B=0 C=1]"])
+print("These should be greater than 1:")
+print(solution_x["P[A=0]"])
+print(solution_x["P[A=0 B=1]"])
+# print(solution_x["P[A=0 B=2]"])
+# print(solution_x["P[A=1 B=2]"])
+# print(solution_x["P[A=0 C=1]"])
+# print(solution_x["P[A=0 C=1]"])
+# print(solution_x["P[A=1 C=2]"])
+print(solution_x["P[A=0 B=1 C=2]"])
+
+
+
+# triangleSDP = InflationSDP(triangle, verbose=2, include_all_outcomes=False, supports_problem=True)
+# triangleSDP.generate_relaxation("local1")
+# triangleSDP.set_distribution(victors_distribution, use_lpi_constraints=True)
+# triangleSDP.solve()
+# print(triangleSDP.status)
+
+# for moment in triangleSDP.atomic_monomials:
+#     if moment.n_operators < 3:
+#         print(moment)
+#
+# triangleSDP.set_bounds({
+#     "P[A=0]": 1,
+#     "P[A=1]": 1,
+#     "P[A=2]": 1}, bound_type="lo")
+# triangleSDP.update_values({
+#     "P[A=0 B=0]": 0,
+#     "P[A=1 B=1]": 0,
+#     "P[A=2 B=2]": 0
+# }, use_lpi_constraints=True)
+# triangleSDP.solve()
